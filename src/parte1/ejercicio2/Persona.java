5c646fc406a5182b223de9b98edebbd7a3583073
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-02-03 12:43:40 +0100

    Ejercicio 2 terminado.

---
+++ src/parte1/ejercicio2/Persona.java
@@ -0,0 +1,8 @@
+package parte1.ejercicio2;
+
+public class Persona {
+	int dni;
+	String nombre;
+	String apellidos;
+	int edad;
+}
---
+++ src/parte1/ejercicio2/PrincipalPersona.java
@@ -0,0 +1,40 @@
+package parte1.ejercicio2;
+
+import java.util.*;
+
+public class PrincipalPersona {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+
+		Persona persona1 = new Persona();
+		Persona persona2 = new Persona();
+		
+		System.out.print("Introduce tu dni: ");
+		persona1.dni = reader.nextInt();
+		reader.nextLine();
+		System.out.print("Introduce tu nombre: ");
+		persona1.nombre = reader.nextLine();
+		System.out.print("Introduce tus apellidos: ");
+		persona1.apellidos = reader.nextLine();
+		System.out.print("Introduce tu edad: ");
+		persona1.edad = reader.nextInt();
+		reader.nextLine();
+		
+		System.out.print("Introduce tu dni: ");
+		persona2.dni = reader.nextInt();
+		reader.nextLine();
+		System.out.print("Introduce tu nombre: ");
+		persona2.nombre = reader.nextLine();
+		System.out.print("Introduce tus apellidos: ");
+		persona2.apellidos = reader.nextLine();
+		System.out.print("Introduce tu edad: ");
+		persona2.edad = reader.nextInt();
+		reader.nextLine();
+		
+		System.out.println(persona1.nombre + " " + persona1.apellidos + " con DNI " + persona1.dni + (persona1.edad >= 18 ? " es mayor de edad." : " no es mayor de edad."));
+		System.out.println(persona2.nombre + " " + persona2.apellidos + " con DNI " + persona2.dni + (persona2.edad >= 18 ? " es mayor de edad." : " no es mayor de edad."));
+		
+	}
+
+}
