86c40d315134df1b5169c6c70b0aee925fc9a63c
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2025-02-16 16:08:58 +0100

    Ejercicio 2 terminado.

---
+++ src/parte3/ejercicio2/Media.java
@@ -0,0 +1,11 @@
+package parte3.ejercicio2;
+
+public class Media {
+	public double media(int num1, int num2) {
+		return (double) (num1 + num2) / 2;
+	}
+	
+	public double media(int num1, int num2, int num3) {
+		return (double) (num1 + num2 + num3) / 3;
+	}
+}
---
+++ src/parte3/ejercicio2/PrincipalMedia.java
@@ -0,0 +1,14 @@
+package parte3.ejercicio2;
+
+public class PrincipalMedia {
+
+	public static void main(String[] args) {
+		Media media1 = new Media();
+		
+		System.out.println(media1.media(3, 4));
+		
+		System.out.println(media1.media(3, 4, 3));
+
+	}
+
+}
