8c501ab00c32f6c6fd26c13dcd927830da282845
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2025-02-16 21:36:16 +0100

    Ejercicio 6 NO terminado (falta fibonacci).

---
+++ src/parte3/ejercicio6/FuncionesMatematicas.java
@@ -0,0 +1,21 @@
+package parte3.ejercicio6;
+
+public class FuncionesMatematicas {
+	
+	public static int sumatorio(int num) {
+		int suma;
+		
+		if (num == 1) {
+			suma = 1;
+		} else {
+			suma = num + sumatorio(num - 1);
+		}
+		
+		return suma;
+	}
+	
+	public static double potencia(double num, int potencia) {
+		return Math.pow(num, potencia);
+	}
+	
+}
---
+++ src/parte3/ejercicio6/PrincipalFuncionesMatematicas.java
@@ -0,0 +1,11 @@
+package parte3.ejercicio6;
+
+public class PrincipalFuncionesMatematicas {
+
+	public static void main(String[] args) {
+		System.out.println(FuncionesMatematicas.sumatorio(5));
+		
+		System.out.println(FuncionesMatematicas.potencia(2, 3));
+	}
+
+}
