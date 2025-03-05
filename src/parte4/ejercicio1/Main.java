97b45644eee35281605198f6fdbe5143995e0d26
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-03-05 10:15:15 +0100

    Ejercicio 1 terminado.

--- src/parte4/ejercicio1/Main.java
+++ src/parte4/ejercicio1/Main.java
@@ -3,6 +3,7 @@ package parte4.ejercicio1;
 import java.util.*;
 
 public class Main {
+	static Scanner reader = new Scanner(System.in);
 	
 	static void mostrarOpciones() {
 		System.out.println("ALUMNOS/AS\n"
@@ -17,15 +18,83 @@ public class Main {
 	static boolean checkOpcion(int num) {
 		boolean res = false;
 		
-		if (num)
+		if (num >= 1 && num <= 5) {
+			res = true;
+		}
+		
+		return res;
+	}
+	
+	static String askNombre() {
+		System.out.print("Introduce el nombre: ");
+		return reader.nextLine();
+	}
+	
+	static double askMedia() {
+		System.out.print("Introduce la nota media: ");
+		double media = reader.nextDouble();
+		reader.nextLine();
+		
+		return media;
 	}
 
 	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
 
 		int opcion;
 		
+		String nombre;
+		double notaMedia;
 		
+		do {
+			mostrarOpciones();
+			
+			do {
+				System.out.print("Introduce una opción: ");
+				opcion = reader.nextInt();
+				reader.nextLine();
+			} while (!checkOpcion(opcion));
+			
+			
+			switch (opcion) {
+			case 1 -> {
+				if (!Crud.read()) {
+					System.out.println("No hay alumnos en la lista.");
+				}
+			}
+			case 2 -> {
+				nombre = askNombre();
+				
+				notaMedia = askMedia();
+				
+				Crud.create(nombre, notaMedia);
+			}
+			case 3 -> {
+				nombre = askNombre();
+				
+				notaMedia = askMedia();
+				
+				if (Crud.update(nombre, notaMedia)) {
+					System.out.println("Alumno actualizado.");
+				} else {
+					System.out.println("El alumno no existe.");
+				}
+			}
+			case 4 -> {
+				nombre = askNombre();
+				
+				if (Crud.remove(nombre)) {
+					System.out.println("Alumno eliminado.");
+				} else {
+					System.out.println("El alumno no existe.");
+				}
+			}
+			case 5 -> {
+				System.out.println("Saliendo del programa...");
+			}
+			}
+		} while (opcion != 5);
+		
+		reader.close();
 	}
 
 }
